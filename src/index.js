d131d50c077e7d25860f69fa4686d8e130395ee4
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-16 12:23:08 +0300

    add eventlistener and DOM function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form'); 
+  let input = document.querySelector('#new-task-description');
+  let taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task = input.value.trim();
+
+    if (task) {
+      const listItem = document.createElement("li");
+      listItem.textContent = task;
+
+      taskList.appendChild(listItem);
+
+      input.value = ' ';
+    }
+
+  });
+
 });
+
+
+
+
+
